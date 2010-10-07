44125b22c16c149a1fb7386dc99aca8cebedc794
SilverIce <slifeleaf@gmail.com>
2010-10-07 21:11:42 +0300

    few clients structures added

---
+++ mov_research/containers.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "typedefs.h"
+
+#pragma pack(push,1)
+
+// IDA doesn't supports templates
+
+#define TSBaseArray(T)\
+    struct TSBaseArray_## T \
+    {\
+        void* vtable;\
+        uint32 m_alloc;\
+        uint32 m_count;\
+        T * m_data;\
+    }\
+
+#define TSFixedArray(T)\
+    TSBaseArray(T);\
+    struct TSFixedArray_## T\
+    {\
+        TSBaseArray_## T baseclass_0;\
+    }\
+
+#define TSGrowableArray(T)\
+    TSFixedArray(T);\
+    struct TSGrowableArray_## T\
+    {\
+        TSFixedArray_## T baseclass_0;\
+        uint32 m_chunk;\
+    }\
+
+class Vector3;
+
+TSGrowableArray(float);
+TSGrowableArray(Vector3);
+
+#pragma pack(pop)
