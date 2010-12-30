2504d968c91de780b28520e958aa997fb2fcbb9d
SilverIce <slifeleaf@gmail.com>
2010-12-30 23:45:25 +0200

    fake classes improvements

--- Movement/Object.h
+++ Movement/Object.h
@@ -2,6 +2,21 @@
 
 #ifdef USE_FAKE_CLASSES
 
+#include "typedefs.h"
+#include "ByteBuffer.h"
+#include "UpdateFields.h"
+
+using Movement::int8;
+using Movement::uint8;
+using Movement::int16;
+using Movement::uint16;
+using Movement::int32;
+using Movement::uint32;
+
+using Movement::uint64;
+using Movement::int64;
+
+
 class WorldPacket;
 class ByteBuffer;
 class Player;
@@ -14,6 +29,14 @@ public:
 
     void SendMessageToSet(WorldPacket *, bool){}
     void SendMessageToSetExcept(WorldPacket *, const Player*){}
+
+    float GetFloatValue(uint16) const { return 0.f;}
+    void SetFloatValue(uint16,float)  {}
+
+    float GetUInt64Value(uint16) const { return 0.f;}
+    void SetUInt64Value(uint16,uint64)  {}
+
+    uint64 GetGUID() const { return 0; }
 
     ByteBuffer m_packGuid;
 };
