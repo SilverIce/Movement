17c561a294e1ef91dcb8e617826920a60fdd3019
SilverIce <slifeleaf@gmail.com>
2011-09-26 12:53:36 +0300

    drop old, commented code

--- mov_tests/SimpleWorld.cpp
+++ mov_tests/SimpleWorld.cpp
@@ -3,12 +3,14 @@
 
 #include "SimpleWorld.h"
 #include <Windows.h>
+#include <conio.h>
 
 extern void test();
 
 int main()
 {
     test();
+    getch();
     //Sleep(0xFFFFFF);
     return 0;
 }
@@ -59,76 +61,6 @@ World::World()
 {
     SLEEP_TIME = def_sleep_time;
 }
-
-/*
-//#include "MoveTables.h"
-#include "PathMover.h"
-#include "MoveData.h"
-
-using namespace Movement;
-
-
-struct MoveTest : public TestArea
-{
-
-   MoveTest() : mdata(0), mover(mdata) {}
-
-   MoveData mdata;
-   NodeMover mover;
-
-   void InitTest()
-   {
-       mdata.SetSpeed(MOVE_WALK,2.5);
-       mdata.SetMoveType(MOVE_WALK);
-
-       PathInitializer init(mdata);
-
-       float step = 2.0f;
-
-       for(float x = 0.0f; x < 16; x += step)
-       {
-           float y = x*2;
-           init.PushPoint( Vector3(x,y,0) );
-           t_log.Out("X %f Y %f", x, y);
-       }
-
-       init.PreparePath();
-
-       mover.Initialize();
-
-       t_log.Out("\n Log positions each 100 ms, full movetime %u", mdata.move_time_full);
-   }
-
-   void Update(const uint32 & diff)
-   {
-       if(mover.Update_short(diff))
-           stop();
-
-       Vector3& pos = mdata.curr_pos;
-       t_log.Out("    x %f y %f z %f time %u", pos.x, pos.y, pos.z, mdata.move_time_passed);
-   }
-};
-
-
-
-struct Corrd : public TestArea
-{
-        //double x=-nan(0x7fffff), y=2.74654499e-42, z=1.32813939e-18;
-    void InitTest(){}
-    void Update(const uint32 & diff)
-    {
-        uint32 crazy_x = ~0x7fffff;
-        float x = *(float*)&crazy_x;
-        if(MaNGOS::IsValidMapCoord(x))
-            t_log.Out("is valid %f",x);
-        else
-            t_log.Out("invalid %f",x);
-
-        stop();
-    }
-
-};*/
-
 
 World& World::instance()
 {
